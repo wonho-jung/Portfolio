a2f7098d1a82097bed229d7813ffaa296d1f3dc3
wonho-jung <wonho.jung92@gmail.com>
2021-04-04 16:31:44 -0400

    sketch...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Header />
+      <Home />
+    </AppContainer>
   );
 }
 
 export default App;
+const AppContainer = styled.div`
+  margin: 0;
+`;
---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+function Home() {
+  return (
+    <HomeContainer>
+      <h1>hi</h1>
+    </HomeContainer>
+  );
+}
+
+export default Home;
+
+const HomeContainer = styled.div`
+  height: 100vh;
+  background-color: #0d0d0d;
+`;
